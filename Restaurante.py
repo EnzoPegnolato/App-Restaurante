7f7c3301f120d20bbef876409c1b5f5424e794d1
Enzo Pegnolato Rodrigues <65430004+EnzoPegnolato@users.noreply.github.com>
2020-09-09 17:34:13 -0300

    Update Restaurante.py

--- Restaurante.py
+++ Restaurante.py
@@ -33,7 +33,7 @@ class JanelaDeLogin():
                 host='localhost',
                 user='root',
                 password='',
-                db='restaurante',
+                db='erp',
                 charset='utf8mb4',
                 cursorclass=pymysql.cursors.DictCursor
             )
